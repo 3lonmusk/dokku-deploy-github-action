bf4b7bcc47064c14cdff4e3ee76f6c4ef770d3b4
3lonmusk <71653684+3lonmusk@users.noreply.github.com>
2020-10-15 20:39:09 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,12 +17,12 @@ ssh-keyscan $DOKKU_HOST >> ~/.ssh/known_hosts
 # Setup the git environment
 git_repo="$DOKKU_USER@$DOKKU_HOST:$DOKKU_APP_NAME"
 cd "$GITHUB_WORKSPACE"
-git remote add deploy "$git_repo"
+git remote add dokku "$git_repo"
 
 # Prepare to push to Dokku git repository
 REMOTE_REF="$GITHUB_SHA:refs/heads/$DOKKU_REMOTE_BRANCH"
 
-GIT_COMMAND="git push deploy $REMOTE_REF $GIT_PUSH_FLAGS -f"
+GIT_COMMAND="git push dokku $REMOTE_REF $GIT_PUSH_FLAGS"
 echo "GIT_COMMAND=$GIT_COMMAND"
 
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 55000" $GIT_COMMAND
